bb394d94a1dab90488c8777596bdf99aa6cb4840
Jérôme Barbier <jerome.barbier@grenoble-inp.org>
2017-01-03 18:00:12 +0100

    Version of the script that is able to run the microservice, not debugged

--- vm_tools/vm_init.sh
+++ vm_tools/vm_init.sh
@@ -32,5 +32,23 @@ else
 	echo "$DATE: Deployment finished with success"
 fi
 
-echo "Run IP notifier..." >> $LOG
-./vm_tools/init_ip_notifier.sh
+echo "Starting the microservice..." >> $LOG
+if [ -z "$MICSERV" ]; then
+    echo " ... No microservice to start, variable \$MICSERV not defined"
+    exit 1
+fi
+
+# Run the builing of the Microservice
+cd microservices
+chmod +x build_container.sh >> $LOG
+
+echo "Run container building..." >> $LOG
+./build_container.sh "$MICSERV" >> $LOG
+
+if [ $? -neq 0 ]; then
+    echo " ... Failed to build container"
+    exit 2
+fi
+
+echo "Run the container..." >> $LOG
+sudo docker run -p 8090:8090 "$MICSERV-service" >> $LOG
