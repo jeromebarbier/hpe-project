8960e5e93b7dc832a90dce57d6f63436ecebe199
victorvalladier <victorvalladier@gmail.com>
2017-01-05 15:14:41 +0100

    Create raw s service

---
+++ microservices/s/config.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+
+import configparser
+
+# Initialise global variable
+logger = None
+s = None
+
+
+def initialise_s():
+    """Define s global object so it can be called from anywhere."""
+    global s
+    s = S()
+
+
+class S(object):
+    def __init__(self):
+        self.NAME = "Microservice s"
+        self.VERSION = "0.1"
+        #
+        # Configuration file
+        self.conf_file = bConfiguration("s.conf")
+
+
+class sConfiguration(object):
+
+    def __init__(self, configuration_file):
+        self.config = configparser.ConfigParser(allow_no_value=True)
+        self.config.read(configuration_file)
+
+    def get_s_port(self):
+        return self.config.get("s", "port")
+
+    def get_s_tmpfile(self):
+        return self.config.get("s", "tmpfile")
+
+    def get_s_tempo(self):
+        return self.config.get("s", "tempo")
+
+    def get_s_debug(self):
+        return self.config.get("s", "debug")
