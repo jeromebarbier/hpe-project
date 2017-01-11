bea6cf5d3c28f3bb2232131d91cff75d2d7163c7
Jérôme Barbier <jerome.barbier@grenoble-inp.org>
2017-01-11 11:02:55 +0100

    Added script to read microservice's IP address from SWIFT

---
+++ microservices/rp/retrieve_ip.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# This script try to retrieve an IP address based on the microservice
+# name
+
+# Call : retrieve_ip.py <SERVICE NAME>
+
+import sys
+
+from lwswift.lwswift import lwswift
+
+service = sys.argv[1]
+
+lws = lwswift()
+ip = lws.get_service(service)
+
+if (ip != None):
+    printf(ip)
