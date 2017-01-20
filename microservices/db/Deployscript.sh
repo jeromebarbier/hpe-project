91e4076a15071f93adc020f7f1b8fb699a985fbc
Jérôme Barbier <jerome.barbier@grenoble-inp.org>
2017-01-20 15:58:23 +0100

    Added instructions to configure and initiate the DB

--- microservices/db/Deployscript.sh
+++ microservices/db/Deployscript.sh
@@ -4,6 +4,21 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get install -y mysql-server
 
+# Create a new user
+USERNAME="admin"
+PASSWORD="admin"
 service mysql restart
-mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin'"
+mysql -uroot -e "CREATE USER '$USERNAME'@'%' IDENTIFIED BY '$PASSWORD'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%'"
+
+# Update configuration to allow external connections
+CONFIG_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
+MYSQL_CONFIG=$(cat $CONFIG_FILE)
+echo "$MYSQL_CONFIG" | grep -L "bind-address " > $CONFIG_FILE
+
+service mysql restart
+
+# Set-up the DB
+mysql -u$USERNAME -p$PASSWORD < prestashop_fullcustomer.dump.sql
+
+exit $?
